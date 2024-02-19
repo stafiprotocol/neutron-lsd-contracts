4a907d098b3239e2e10e6416497312b39e72aa92
Chengbin Du <robertduu@gmail.com>
2024-02-19 14:54:30 +0800

    remove config total stake fee

--- contracts/stake_manager/src/execute_config_stack.rs
+++ contracts/stake_manager/src/execute_config_stack.rs
@@ -22,9 +22,6 @@ pub fn execute_config_stack(
     if let Some(stack_fee_commission) = param.stack_fee_commission {
         stack.stack_fee_commission = stack_fee_commission;
     }
-    if let Some(total_stack_fee) = param.total_stack_fee {
-        stack.total_stack_fee = total_stack_fee;
-    }
     if let Some(new_admin) = param.new_admin {
         stack.admin = new_admin;
     }
